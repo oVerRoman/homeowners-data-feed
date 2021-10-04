a877f5a01d04138ce3c41bb6263580d9b4e2c7f9
Roman Vershinin <romverveter@gmail.com>
2021-10-04 20:06:43 +0400

    Fix merge conflict in class AdminController()

--- src/main/java/com/simbirsoftintensiv/intensiv/controller/AdminController.java
+++ src/main/java/com/simbirsoftintensiv/intensiv/controller/AdminController.java
@@ -1,6 +1,5 @@
 package com.simbirsoftintensiv.intensiv.controller;
 
-import com.simbirsoftintensiv.intensiv.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,11 +8,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.simbirsoftintensiv.intensiv.service.UserService;
+
 @Controller
 public class AdminController {
+
     @Autowired
     private UserService userService;
- 
+
 //Доступ к странице admin имеют только пользователи с ролью администратора.
     @GetMapping("/admin")
     public String userList(Model model) {
@@ -22,29 +24,16 @@ public class AdminController {
     }
 
     @PostMapping("/admin")
-<<<<<<< src/main/java/com/simbirsoftintensiv/intensiv/controller/AdminController.java
     public String deleteUser(@RequestParam(required = true, defaultValue = "") Long userId,
-                             @RequestParam(required = true, defaultValue = "") String action,
-                             Model model) {
-        // Вместо анотации использовать PathVariable ???
+            @RequestParam(required = true, defaultValue = "") String action, Model model) {
         if (action.equals("delete")) {
-=======
-    public String  deleteUser(@RequestParam(required = true, defaultValue = "" ) Long userId,
-                              @RequestParam(required = true, defaultValue = "" ) String action,
-                              Model model) {
-        if (action.equals("delete")){
->>>>>>> src/main/java/com/simbirsoftintensiv/intensiv/controller/AdminController.java
             userService.deleteUser(userId);
         }
         return "redirect:/admin";
     }
 
     @GetMapping("/admin/gt/{userId}")
-<<<<<<< src/main/java/com/simbirsoftintensiv/intensiv/controller/AdminController.java
     public String gtUser(@PathVariable("userId") Long userId, Model model) {
-=======
-    public String  gtUser(@PathVariable("userId") Long userId, Model model) {
->>>>>>> src/main/java/com/simbirsoftintensiv/intensiv/controller/AdminController.java
         model.addAttribute("allUsers", userService.usergtList(userId));
         return "admin";
     }
