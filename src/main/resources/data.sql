a7e73bebe46aa49345854a5a42e6464f6186de5c
ILYA <sokolovnnov@gmail.com>
2021-10-08 16:06:47 +0300

    add data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,25 @@
 INSERT INTO public.companies (id, name, address, phone)
 VALUES (50000, 'ТСЖ', 'Чикаго', 222333);
 
-INSERT INTO public.users (id, phone, email, first_name, second_name, patronymic, address, company_id)
-VALUES (60000, 70000000000, 'user0@mail.ru', 'Петр', 'Петров', 'Петрович', 0, 50000);
-INSERT INTO public.users (id, phone, email, first_name, second_name, patronymic, address, company_id)
-VALUES (60001, 71111111111, 'user1@mail.ru', 'Сидор', 'Сидоров', 'Сидорович', 1, 50000);
-INSERT INTO public.users (id, phone, email, first_name, second_name, patronymic, address, company_id)
-VALUES (60002, 72222222222, 'user2@mail.ru', 'Иван', 'Иванов', 'Иванович', 2, 50000);
-INSERT INTO public.users (id, phone, email, first_name, second_name, patronymic, address, company_id)
-VALUES (60003, 73333333333, 'user3@mail.ru', 'Фирс', 'Фирсов', 'Фирсович', 3, 50000);
+INSERT INTO public.addresses (id, city, street, house, building, apartment) VALUES (80002, 'Чикаго', 'улица Ленина', '1', null, '31');
+INSERT INTO public.addresses (id, city, street, house, building, apartment) VALUES (80000, 'Чикаго', 'улица Ленина', '1', null, '11');
+INSERT INTO public.addresses (id, city, street, house, building, apartment) VALUES (80003, 'Чикаго','улица Ленина', '3', 'A',  '3');
+INSERT INTO public.addresses (id, city, street, house, building, apartment) VALUES (80001, 'Чикаго', 'улица Ленина', '1', null, '21');
+
+INSERT INTO public.users (id, phone, email, first_name, second_name, patronymic, address_id, company_id)
+VALUES (60000, 70000000000, 'user0@mail.ru', 'Петр', 'Петров', 'Петрович', 80002, 50000);
+INSERT INTO public.users (id, phone, email, first_name, second_name, patronymic, address_id, company_id)
+VALUES (60001, 71111111111, 'user1@mail.ru', 'Сидор', 'Сидоров', 'Сидорович', 80000, 50000);
+INSERT INTO public.users (id, phone, email, first_name, second_name, patronymic, address_id, company_id)
+VALUES (60002, 72222222222, 'user2@mail.ru', 'Иван', 'Иванов', 'Иванович', 80001, 50000);
+INSERT INTO public.users (id, phone, email, first_name, second_name, patronymic, address_id, company_id)
+VALUES (60003, 73333333333, 'user3@mail.ru', 'Фирс', 'Фирсов', 'Фирсович', 80003, 50000);
+
+INSERT INTO user_roles (role, user_id)
+VALUES ('USER', 60000),
+       ('USER', 60001),
+       ('USER', 60003),
+       ('USER', 60002);
 
 INSERT INTO public.maters (id, name, user_id) VALUES (70000, 'эл.юз1', 60000);
 INSERT INTO public.maters (id, name, user_id) VALUES (70001, 'г.вод.юз1', 60000);
@@ -37,7 +48,4 @@ INSERT INTO public.mater_values (id, mater_id, date, value) VALUES (80013, 70006
 INSERT INTO public.mater_values (id, mater_id, date, value) VALUES (80014, 70007, '2021-09-06 17:30:45.000000', 800);
 INSERT INTO public.mater_values (id, mater_id, date, value) VALUES (80015, 70007, '2021-10-06 17:30:45.000000', 810);
 
-INSERT INTO public.addresses (id, user_id, city, street, house, building, apartment) VALUES (80002, 60002, 'Чикаго', 'улица Ленина', '1', null, '31');
-INSERT INTO public.addresses (id, user_id, city, street, house, building, apartment) VALUES (80000, 60000, 'Чикаго', 'улица Ленина', '1', null, '11');
-INSERT INTO public.addresses (id, user_id, city, street, house, building, apartment) VALUES (80003, 60003, 'Чикаго','улица Ленина', '3', 'A',  '3');
-INSERT INTO public.addresses (id, user_id, city, street, house, building, apartment) VALUES (80001, 60001, 'Чикаго', 'улица Ленина', '1', null, '21');
+
