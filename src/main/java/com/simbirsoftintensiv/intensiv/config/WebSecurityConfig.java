a6bc49a2ddc06c8481d47fa176af34de836de5eb
aevte <aevtefeev@yandex.ru>
2021-10-17 13:20:31 +0300

    Добавлена поддержка CORS.

--- src/main/java/com/simbirsoftintensiv/intensiv/config/WebSecurityConfig.java
+++ src/main/java/com/simbirsoftintensiv/intensiv/config/WebSecurityConfig.java
@@ -16,8 +16,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.annotation.PostConstruct;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -98,8 +105,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .defaultSuccessUrl("/").permitAll()
         ;
 
+        // Евтефеев - Добавил поддержку запросов CORS
+        httpSecurity.cors(withDefaults());
+
         httpSecurity.exceptionHandling()
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+    }
+
+    // Евтефеев - Настройки запросов CORS. Разрешены все запросы с любого адреса
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Override
