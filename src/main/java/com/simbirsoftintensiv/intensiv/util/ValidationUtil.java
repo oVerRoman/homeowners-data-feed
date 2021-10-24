860a1b4c7f5ee12e4fd91b0e9b38028f502b4a83
ILYA <sokolovnnov@gmail.com>
2021-10-24 17:26:31 +0300

    delete double check user in Db

--- src/main/java/com/simbirsoftintensiv/intensiv/util/ValidationUtil.java
+++ src/main/java/com/simbirsoftintensiv/intensiv/util/ValidationUtil.java
@@ -4,32 +4,31 @@ package com.simbirsoftintensiv.intensiv.util;
 import com.simbirsoftintensiv.intensiv.entity.HasId;
 import com.simbirsoftintensiv.intensiv.entity.User;
 import com.simbirsoftintensiv.intensiv.exception_handling.NotFoundException;
-import com.simbirsoftintensiv.intensiv.to.UserTo;
 
 public class ValidationUtil {
 
-    public static void checkNotFoundWithId(boolean found, int id) {
-        checkNotFound(found, "id=" + id);
-    }
+    //    public static void checkNotFoundWithId(boolean found, int id) {
+//        checkNotFound(found, "id=" + id);
+//    }
 
     public static void checkNotFoundWithPhone(boolean found, long phone) {
         checkNotFound(found, "id=" + phone);
     }
 
-    public static <T> T checkNotFoundWithId(T object, int id) {
-        checkNotFoundWithId(object != null, id);
-        return object;
-    }
+//    public static <T> T checkNotFoundWithId(T object, int id) {
+//        checkNotFoundWithId(object != null, id);
+//        return object;
+//    }
 
     public static User checkNotFoundWithPhone(User user, long phone) {
         checkNotFoundWithPhone(user != null, phone);
         return user;
     }
 
-    public static <T> T checkNotFound(T object, String msg) {
-        checkNotFound(object != null, msg);
-        return object;
-    }
+//    public static <T> T checkNotFound(T object, String msg) {
+//        checkNotFound(object != null, msg);
+//        return object;
+//    }
 
     public static void checkNotFound(boolean found, String msg) {
         if (!found) {
@@ -43,18 +42,15 @@ public class ValidationUtil {
         }
     }
 
-    public static void checkPhoneEquality(UserTo userTo, long phone) {
-        if (Long.parseLong(userTo.getPhone()) != phone) {
+    public static void checkPhoneEquality(long updatePhone, long phone) {
+        if (updatePhone != phone) {
             throw new NotFoundException("User cannot change phone number! Only the administrator can..");
         }
     }
 
     public static void checkPhone(boolean found) {
         if (found) {
-            // fixme change exception
             throw new NotFoundException("Phone number already exists!");
         }
     }
-
-
 }
