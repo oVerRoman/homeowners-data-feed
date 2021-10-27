93b38a1651d400caf650f29a90993f64afc4d55f
Dmitry Plodukhin <dmitry.plodukhin@simbirsoft.com>
2021-10-27 19:02:43 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo git pull origin master
-sudo sh mvnw spring-boot:run -Dspring-boot.run.jvmArguments=-Djasypt.encryptor.password=123 -f pom.xml -Dspring-boot.run.arguments="--server.port=443 —spring.datasource.url=jdbc:postgresql://localhost:5432/hodf —spring.datasource.password=postgres"
+sudo sh mvnw spring-boot:run -Dspring-boot.run.jvmArguments=-Djasypt.encryptor.password=123 -f pom.xml -Dspring-boot.run.arguments="--server.port=443 --spring.datasource.url=jdbc:postgresql://localhost:5432/hodf --spring.datasource.password=postgres"
 
 
 
